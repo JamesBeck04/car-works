c21c7e3657471cd69ce19a9fb29f1a68ac92aeb6
JamesBeck04 <123965385+JamesBeck04@users.noreply.github.com>
2023-11-02 02:54:17 +0000

    Leaderboard2
    2

--- Assets/Scripts/Leaderboard/LeaderBoard.cs
+++ Assets/Scripts/Leaderboard/LeaderBoard.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Constraints;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -10,7 +11,8 @@ public class LeaderBoard : MonoBehaviour
 
     private Transform entryContainer;
     private Transform entryTemplate;
-    float time = 0f;
+    private List<Transform> highscoreEntryTransformList;
+
 
     private void Awake()
     {
@@ -19,50 +21,122 @@ public class LeaderBoard : MonoBehaviour
 
         entryTemplate.gameObject.SetActive(false);
 
-     
-        for (int i = 0; i < 5; i++)
+        /*highscoreEntryList = new List<HighscoreEntry>()
         {
-           
-        }
+            new HighscoreEntry{ time = 300, name = "Bob"},
+            new HighscoreEntry{ time = 567, name = "Mike"},
+            new HighscoreEntry{ time = 900, name = "Jeff"},
+            new HighscoreEntry{ time = 234, name = "Garry"},
+            new HighscoreEntry{ time = 562, name = "Rob"},
+        };*/
+
+
+        //AddHighscoreEntry(400, "ABC");
+
+
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+    
+
+
+            for (int i = 0; i < highscores.highscoreEntryList.Count; i++)
+            {
+                for (int j = i + 1; j < highscores.highscoreEntryList.Count; j++)
+                {
+                    if (highscores.highscoreEntryList[j].time < highscores.highscoreEntryList[i].time)
+                    {
+                        HighscoreEntry tmp = highscores.highscoreEntryList[i];
+                        highscores.highscoreEntryList[i] = highscores.highscoreEntryList[j];
+                        highscores.highscoreEntryList[j] = tmp;
+                    }
+                }
+            }
+
+
+
+        Debug.Log(highscores.highscoreEntryList.Count);
+
+
+        highscoreEntryTransformList = new List<Transform>();
+       
+            foreach (HighscoreEntry highscoreEntry in highscores.highscoreEntryList)
+            {
+
+            
+                CreateLeaderBoardEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+
+
+            }
+        
+     
+
+        /*Highscores highscores = new Highscores { highscoreEntryList = highscoreEntryList };
+        string json = JsonUtility.ToJson(highscores);
+        PlayerPrefs.SetString("highscoreTable", json);
+        PlayerPrefs.Save();
+        Debug.Log(PlayerPrefs.GetString("highscoreTable"));
+      */
     }
 
     private void CreateLeaderBoardEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
     {
         float templateHeight = 50f;
-        Transform entryTransform = Instantiate(entryTemplate, container);
-        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
-        entryTransform.gameObject.SetActive(true);
+      
+            Transform entryTransform = Instantiate(entryTemplate, container);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+            entryTransform.gameObject.SetActive(true);
 
-        int rank = transformList.Count + 1;
-        string rankString;
+            int rank = transformList.Count + 1;
+            string rankString;
 
-        switch (rank)
-        {
-            default:
-                rankString = rank + "TH"; break;
+            switch (rank)
+            {
+                default:
+                    rankString = rank + "TH"; break;
 
-            case 1: rankString = "1ST"; break;
-            case 2: rankString = "2ND"; break;
-            case 3: rankString = "3RD"; break;
-        }
+                case 1: rankString = "1ST"; break;
+                case 2: rankString = "2ND"; break;
+                case 3: rankString = "3RD"; break;
+            }
 
-        entryTransform.Find("Pos").GetComponent<TextMeshProUGUI>().text = rankString;
+            entryTransform.Find("Pos").GetComponent<TextMeshProUGUI>().text = rankString;
 
-        float time = highscoreEntry.time;
-        int minutes = (int)(time / 60) % 60;
-        int seconds = (int)(time % 60);
+            float time = highscoreEntry.time;
+            int minutes = (int)(time / 60) % 60;
+            int seconds = (int)(time % 60);
 
 
-        entryTransform.Find("Time").GetComponent<TextMeshProUGUI>().text = minutes.ToString() + "m " + seconds.ToString() + "s ";
+            entryTransform.Find("Time").GetComponent<TextMeshProUGUI>().text = minutes.ToString() + "m " + seconds.ToString() + "s ";
 
-        string name = highscoreEntry.name;
-        entryTransform.Find("Name").GetComponent<TextMeshProUGUI>().text = name;
+            string name = highscoreEntry.name;
+            entryTransform.Find("Name").GetComponent<TextMeshProUGUI>().text = name;
 
-        transformList.Add(entryTransform);
+            transformList.Add(entryTransform);
+        
     }
 
 
+    private void AddHighscoreEntry(float time, string name)
+    {
+        HighscoreEntry highscoreEntry = new HighscoreEntry { time = time, name = name };
+
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+        highscores.highscoreEntryList.Add(highscoreEntry);
+        string json = JsonUtility.ToJson(highscores);
+        PlayerPrefs.SetString("highscoreTable", json);
+        PlayerPrefs.Save();
+    }
+
+
+    private class Highscores
+    {
+        public List<HighscoreEntry> highscoreEntryList;
+    }
+
+    [System.Serializable]
     private class HighscoreEntry {
         public float time;
         public string name;
